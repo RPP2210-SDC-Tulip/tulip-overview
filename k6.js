a02bf08035024b3c6687583512f304c1b5d16eb6
fmbandit <itshardtosaynotknowing@gmail.com>
2023-05-18 17:58:27 -0700

    simplified k6 function: simply change the 'selected' variable to test a different route

--- k6.js
+++ k6.js
@@ -3,12 +3,29 @@ import { sleep } from 'k6';
 
 export const options = {
     vus: 5000,
-    duration: '1200s',
-    rps: 2500
+    duration: '30s',
+    rps: 2700
 };
 
 export default function () {
-    const productId = Math.floor(Math.random() * (1000011 - 1) + 1);
-    http.get(`http://localhost:3000/products/${productId}/related`);
+    // changing endpoint below should be all you need to test a different route
+    const selected = 'getProducts'; 
+    const apiUrl = 'http://localhost:3000/';
+    const ranges = {
+        products: 1000011,  // total records in db
+        pages: 200002,      // (total records / 5) [should lower this if count > 5]
+    };
+    const random = {
+        product: Math.floor(Math.random() * (ranges.products - 1) + 1),
+        page: Math.floor(Math.random() * (ranges.pages - 1) + 1),
+    };
+    const endpoints = {
+        getProduct: () => `${apiUrl}products/${random.product}`,
+        getProducts: () => `${apiUrl}products?page=${random.page}`,
+        getStyles: () => `${apiUrl}products/${random.product}/styles`,
+        getRelated: () => `${apiUrl}products/${random.product}/related`,
+    };
+    http.get(endpoints[selected]());
     sleep(1);
+
 }
